aec229b47b48b85767a31bf086d21115e578afc8
Thalisson Castro <adaothalisson@hotmail.com>
2021-08-31 16:39:08 -0300

    Implementando algoritmo DDA.

--- Projetos/Comp gráfica/Atividade 2/script.js
+++ Projetos/Comp gráfica/Atividade 2/script.js
@@ -4,18 +4,13 @@ let point3; //Pontos reta
 let point4;
 
 let point5; //Pontos circulo
-let point6; 
+let point6;
 let point7;
 
-function ReceberValoresReta(){
-    point1 = document.querySelector("#idponto1").value; 
-    point2 = document.querySelector("#idponto2").value; 
-    point3 = document.querySelector("#idponto3").value; 
-    point4 = document.querySelector("#idponto4").value; 
-    PlotarReta();
-}  
+let pixel = "";
+var tela = document.querySelector(".prompt");
 
-function PlotarReta(){
+function PlotarReta() {
     let camp1 = document.querySelector("#reta");
     camp1.setAttribute("x1", point1);
     camp1.setAttribute("y1", point2);
@@ -23,16 +18,51 @@ function PlotarReta(){
     camp1.setAttribute("y2", point4);
 }
 
-function ReceberValoresCirculo(){
-    point5 = document.querySelector("#idcentroX").value; 
-    point6 = document.querySelector("#idcentroY").value; 
-    point7 = document.querySelector("#idraio").value; 
+function ReceberValoresCirculo() {
+    point5 = document.querySelector("#idcentroX").value;
+    point6 = document.querySelector("#idcentroY").value;
+    point7 = document.querySelector("#idraio").value;
     PlotarCirculo();
-}  
+}
 
-function PlotarCirculo(){
+function PlotarCirculo() {
     let camp2 = document.querySelector("#circulo");
     camp2.setAttribute("cx", point5);
     camp2.setAttribute("xy", point6);
     camp2.setAttribute("r", point7);
+}
+
+function drawPixel(x, y) {
+    pixel += `<rect x="${x}" y= "${y}" width="5" height="5" style="fill:red; stroke:black; stroke-width:3px"/>`;
+}
+
+
+//retorna um numero positivo
+
+function abs(number) {
+    if (number < 0)
+        return number * (-1)
+    return number
+}
+//arredonda o número
+
+function round(number) {
+    return parseInt(number + 0.5);
+}
+
+
+function DDA(x1, y1, x2, y2){
+    let step;
+    let X;                      //COMECEI A FAZER IGUAL A DE JORGE
+    tela.innerHTML = pixel;
+}
+
+
+
+function ReceberValoresReta() {
+    point1 = document.querySelector("#idponto1").value;
+    point2 = document.querySelector("#idponto2").value;
+    point3 = document.querySelector("#idponto3").value;
+    point4 = document.querySelector("#idponto4").value;
+    DDA(point1, point2, point3, point4);
 }
