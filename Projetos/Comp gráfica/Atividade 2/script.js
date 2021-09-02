276b0964c1a1c58b3c954cdab6a8e7fee028a1e3
Thalisson Castro <adaothalisson@hotmail.com>
2021-09-01 22:52:01 -0300

    Implementação dos algoritmos para plotagem do circulo.

--- Projetos/Comp gráfica/Atividade 2/script.js
+++ Projetos/Comp gráfica/Atividade 2/script.js
@@ -10,12 +10,17 @@ let point7;
 let pixel = "";
 var tela = document.querySelector(".prompt");
 
-function ReceberValoresCirculo() {
-    point5 = document.querySelector("#idcentroX").value;
-    point6 = document.querySelector("#idcentroY").value;
-    point7 = document.querySelector("#idraio").value;
-    PlotarCirculo();
+function plotPoint(xc, yc, x, y) {
+    drawPixel(xc + x, yc + y);
+    drawPixel(xc - x, yc + y);
+    drawPixel(xc + x, yc - y);
+    drawPixel(xc - x, yc - y);
+    drawPixel(xc + y, yc + x);
+    drawPixel(xc - y, yc + x);
+    drawPixel(xc + y, yc - x);
+    drawPixel(xc - y, yc - x);
 }
+
 
 function PlotarCirculo() {
     let camp2 = document.querySelector("#circulo");
@@ -57,6 +62,7 @@ function DDA(x1, y1, x2, y2) {
         i++;
     }
     tela.innerHTML = pixel;
+    pixel = "";
 
 }
 
@@ -86,11 +92,59 @@ function Bresenham(x1, y1, x2, y2) {
         }
     }
     tela.innerHTML = pixel;
-
-
+    pixel = "";
 }
 
+function CirculoSimples(xc, yc, r) {
+    let x, y;
+    let yr;
 
+    x = 0;
+    y = r;
+    yr = r;
+
+    plotPoint(xc, yc, x, y);
+
+    while (x < yr) {
+        x += 1;
+        yr = Math.sqrt(r * r - x * x);
+        y = parseInt(round(yr));
+        plotPoint(xc, yc, x, y);
+    }
+
+    tela.innerHTML = pixel;
+    pixel = "";
+}
+
+function CirculoPontoMedio(xc, yc, r) {
+    let x, y, p;
+    x = 0;
+    y = r;
+    p = 1 - r;
+
+    plotPoint(xc, yc, x, y);
+
+    while (x < y) {
+        x += 1;
+        if (p < 0) {
+            p = p + 2 * x + 1;
+        } else {
+            y -= 1;
+            p = p + 2 * (x - y) + 1;
+        }
+        plotPoint(xc, yc, x, y);
+    }
+    tela.innerHTML = pixel;
+    pixel = "";
+}
+
+function PlotarReta() {
+    let camp1 = document.querySelector("#reta");
+    camp1.setAttribute("x1", point1);
+    camp1.setAttribute("y1", point2);
+    camp1.setAttribute("x2", point3);
+    camp1.setAttribute("y2", point4);
+}
 
 function ReceberValoresRetaDDA() {
     point1 = parseInt(document.querySelector("#idponto1").value);
@@ -101,10 +155,24 @@ function ReceberValoresRetaDDA() {
     DDA(point1, point2, point3, point4);
 }
 
-function PlotarReta() {
-    let camp1 = document.querySelector("#reta");
-    camp1.setAttribute("x1", point1);
-    camp1.setAttribute("y1", point2);
-    camp1.setAttribute("x2", point3);
-    camp1.setAttribute("y2", point4);
+function RecebeValoresRetaBresenham() {
+    point1 = parseInt(document.querySelector("#idponto1").value);
+    point2 = parseInt(document.querySelector("#idponto2").value);
+    point3 = parseInt(document.querySelector("#idponto3").value);
+    point4 = parseInt(document.querySelector("#idponto4").value);
+    Bresenham(point1, point2, point3, point4);
+}
+
+function ReceberValoresCirculoSimples() {
+    point5 = document.querySelector("#idcentroX").value;
+    point6 = document.querySelector("#idcentroY").value;
+    point7 = document.querySelector("#idraio").value;
+    CirculoSimples(point5, point6, point7);
+}
+
+function ReceberValoresCirculoPontoMedio() {
+    point5 = document.querySelector("#idcentroX").value;
+    point6 = document.querySelector("#idcentroY").value;
+    point7 = document.querySelector("#idraio").value;
+    CirculoPontoMedio(point5, point6, point7);
 }
