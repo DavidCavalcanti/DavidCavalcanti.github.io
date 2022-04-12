6de90ae9b7c2f97830b68d21adc45f011560e498
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-12 14:34:10 -0300

    especificar função criar p

--- JavaScript/assets/js/main.js
+++ JavaScript/assets/js/main.js
@@ -9,11 +9,17 @@ form.addEventListener("submit", function (event) {
   setResutado() ;
 });
 
+// Função que cria parágrafo
+function criaP(className){
+    const p = document.createElement('p'); // Cria um parágrafo(elemento) 
+    return p;
+}
+
 //Função que insere um html dentro da div.resultado
 function setResutado(mensagem) {
   const resultado = document.querySelector('#resultado');
   resultado.innerHTML = ''; // Zera resultado 
-  const p = document.createElement('p'); // Cria um parágrafo(elemento)
+  const p = criaP();  
   p.classList.add('paragrafo-resultado'); // Adiciona uma classe ao parágrafo p
   p.innerHTML = 'Qualquer coisa'; // coloca um html dentro do parágrafo
   resultado.appendChild(p); // insere p ao resultado (insere na div)
