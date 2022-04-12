567e80f790a900bb4fd90104c1c348abb6122664
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-12 15:00:01 -0300

    pegar inputs

--- JavaScript/assets/js/main.js
+++ JavaScript/assets/js/main.js
@@ -5,22 +5,28 @@ const form = document.querySelector("#formulario");
 form.addEventListener("submit", function (event) {
   // '(e)' é o evento que a qual a função captura
   event.preventDefault();
-  console.log("Evento parado"); 
-  setResutado() ;
+
+  //pegando os inputs
+  const inputPeso = event.target.querySelector("#peso"); // .target informa qual elemento está recebendo o evento, qual elemento foi clicado na página
+  const inputAltura = event.target.querySelector("#altura");
+
+  // Pegando os valores dos inputs
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 });
 
 // Função que cria parágrafo
-function criaP(className){
-    const p = document.createElement('p'); // Cria um parágrafo(elemento) 
-    return p;
+function criaP(className) {
+  const p = document.createElement("p"); // Cria um parágrafo(elemento)
+  return p;
 }
 
 //Função que insere um html dentro da div.resultado
 function setResutado(mensagem) {
-  const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = ''; // Zera resultado 
-  const p = criaP();  
-  p.classList.add('paragrafo-resultado'); // Adiciona uma classe ao parágrafo p
-  p.innerHTML = 'Qualquer coisa'; // coloca um html dentro do parágrafo
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = ""; // Zera resultado
+  const p = criaP();
+  p.classList.add("paragrafo-resultado"); // Adiciona uma classe ao parágrafo p
+  p.innerHTML = "Qualquer coisa"; // coloca um html dentro do parágrafo
   resultado.appendChild(p); // insere p ao resultado (insere na div)
 }
