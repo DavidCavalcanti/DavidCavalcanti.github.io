3504e7aece25a27128e30d4135d7ff1a50a9b3b9
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-12 14:25:27 -0300

    criar parágrafo

--- JavaScript/assets/js/main.js
+++ JavaScript/assets/js/main.js
@@ -5,11 +5,16 @@ const form = document.querySelector("#formulario");
 form.addEventListener("submit", function (event) {
   // '(e)' é o evento que a qual a função captura
   event.preventDefault();
-  console.log("Evento parado");  
+  console.log("Evento parado"); 
+  setResutado() ;
 });
 
 //Função que insere um html dentro da div.resultado
 function setResutado(mensagem) {
   const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = '';
+  resultado.innerHTML = ''; // Zera resultado 
+  const p = document.createElement('p'); // Cria um parágrafo(elemento)
+  p.classList.add('paragrafo-resultado'); // Adiciona uma classe ao parágrafo p
+  p.innerHTML = 'Qualquer coisa'; // coloca um html dentro do parágrafo
+  resultado.appendChild(p); // insere p ao resultado (insere na div)
 }
