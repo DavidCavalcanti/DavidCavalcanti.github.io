14d11a4afc5d4d026d8316b2f2e085398bbdc507
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-13 09:34:14 -0300

    validar o nível de imc

--- JavaScript/assets/js/main.js
+++ JavaScript/assets/js/main.js
@@ -26,13 +26,32 @@ form.addEventListener("submit", function (event) {
   }
 
   const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
 
-  console.log(imc);
+  console.log(imc, nivelImc);
 });
 
 function getImc(peso, altura) {
   const imc = peso / altura ** 2;
   return imc.toFixed(2); // retorna com precissão de duas casas decimais
+}
+
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
 }
 
 // Função que cria parágrafo
