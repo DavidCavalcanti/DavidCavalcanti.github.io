e5f9c27fb35b584d7233cba8adf130d05d75a403
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-26 13:36:55 -0300

    Create retornoFuncoes.js

---
+++ Linguagens de programação/JavaScript/funções avançado/retornoFuncoes.js
@@ -0,0 +1,24 @@
+/*function falaFrase (comeco){
+    function falaResto(resto){
+        return  comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const olaMundo = falaFrase('Olá');
+
+console.log(olaMundo('mundo!'));*/
+
+function criaMultiplicador(multiplicador){
+    return function(n){
+        return multiplicador * n;
+    }
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
