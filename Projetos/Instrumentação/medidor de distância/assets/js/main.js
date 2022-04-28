793b113f1f9cbec9c0223e24c6d9e6a5213110a6
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-28 11:35:16 -0300

    Criar função que configura o relógio

--- Projetos/Instrumentação/medidor de distância/assets/js/main.js
+++ Projetos/Instrumentação/medidor de distância/assets/js/main.js
@@ -1,8 +1,15 @@
 function relogio() {
+  function getFromSeconds(seconds) {
+    const data = new Date(seconds * 1000); // JavaScript recebe em ms
+
+    return data.toLocaleTimeString("pt-br", {
+      timeZone: "UTC", // Passando UTC ele desconsidera o horário atual, considerando a partir de 01/01/1970, 00:00:00.
+    });
+  }
   const relogio = document.querySelector(".relogio");
 
   //Pegar envento de click
-  document.addEventListener('click', (evento) => {
-      const elemento = evento.target; // Target informa em qual elemento estamos clicando
-  })
+  document.addEventListener("click", (evento) => {
+    const elemento = evento.target; // Target informa em qual elemento estamos clicando
+  });
 }
