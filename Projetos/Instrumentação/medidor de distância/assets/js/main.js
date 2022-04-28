8dbb6fe8ce0a5644b9cb1ffdfeff52bb49b45de6
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-28 11:25:55 -0300

    backend - Temporizador

---
+++ Projetos/Instrumentação/medidor de distância/assets/js/main.js
@@ -0,0 +1,8 @@
+function relogio() {
+  const relogio = document.querySelector(".relogio");
+
+  //Pegar envento de click
+  document.addEventListener('click', (evento) => {
+      const elemento = evento.target; // Target informa em qual elemento estamos clicando
+  })
+}
