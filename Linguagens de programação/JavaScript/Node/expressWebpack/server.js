1bfa11e7da25dc7291f52adc09ab3bce78f88b1d
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-26 13:52:55 -0300

    Preparar ambiente - Arquivos estáticos

---
+++ Linguagens de programação/JavaScript/Node/expressWebpack/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const path = require("path");
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
+
+app.listen(port, () => {
+  console, console.log("Acessar servidor: http://localhost:3000/");
+  console.log("Servidor ativo na porta 3000");
+});
