f177677caa30e3355706403b17e7713b2f31599f
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-01 11:12:00 -0300

    Preparar ambiente para conexão com o  MongoDB

---
+++ Linguagens de programação/JavaScript/Node/mongoDB/src/controllers/home.js
@@ -0,0 +1,8 @@
+exports.paginaHome = (req, res) => {
+  res.render("home.ejs");
+  /*res.send(`
+       <h1 style="text-align: center"> Hy, I'm David Cavalcanti 
+        <br> Computer enginnering Student at UNIVASF        
+        </h1><hr>                
+    `);*/
+};
