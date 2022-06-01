b30b1840b7fc65577df9008b8b1f09dce02b0834
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-31 23:21:42 -0300

    Importar e usar o middleware
    Agora todas as requisições passam pelo arquivo src/middlewares/middleware.js

--- Linguagens de programação/JavaScript/Node/expressMiddliwares/server.js
+++ Linguagens de programação/JavaScript/Node/expressMiddliwares/server.js
@@ -3,6 +3,7 @@ const app = express();
 const routes = require("./routes");
 const path = require("path");
 const port = 3000;
+const middleware = require("./src/middlewares/middleware");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,6 +12,7 @@ app.use(express.static(path.resolve(__dirname, "public")));
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(middleware);
 app.use(routes);
 
 app.listen(port, () => {
