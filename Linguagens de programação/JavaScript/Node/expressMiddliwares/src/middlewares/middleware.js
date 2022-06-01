b482f4d4055d98c7e0f344d004f4620386754242
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-31 23:23:15 -0300

    Limpar exemplo de teste

--- Linguagens de programação/JavaScript/Node/expressMiddliwares/src/middlewares/middleware.js
+++ Linguagens de programação/JavaScript/Node/expressMiddliwares/src/middlewares/middleware.js
@@ -1,10 +1,3 @@
 module.exports = (req, res, next) => {
-  console.log();
-    if(req.body.cliente) {
-        req.body.cliente = req.body.cliente.replace("Cavalcanti", "Não use Cavalcanti")
-        console.log(`Você postou ${req.body.cliente}`);
-    }
-  console.log();
-
   next();
 };
