4acee906bcb12bc38d3c2e310b63c7873e42ca11
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-02 15:24:04 -0300

    Capturar evento emitido
    Agora a conexão só ocorre após o app emitir o sinal de "pronto".

--- Linguagens de programação/JavaScript/Node/mongoDB/server.js
+++ Linguagens de programação/JavaScript/Node/mongoDB/server.js
@@ -3,13 +3,17 @@ const app = express();
 
 const mongoose = require("mongoose");
 const connectionString =
-"mongodb+srv://davidcavalcanti:teste123@cluster0.c9srumn.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUniFiedTopology: true,
-}).then(() => {
-  app.emit('Pronto');
-});
+  "mongodb+srv://davidcavalcanti:teste123@cluster0.c9srumn.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUniFiedTopology: true,
+  })
+  .then(() => {
+    console.log("Sucesso ao conectar à base de dados.");
+    app.emit("Pronto");
+  })
+  .catch((erro) => console.log(erro));
 
 const routes = require("./routes");
 const path = require("path");
@@ -26,7 +30,9 @@ app.set("view engine", "ejs");
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(port, () => {
-  console, console.log("Acessar servidor: http://localhost:3000/");
-  console.log("Servidor ativo na porta 3000");
+app.on("Pronto", () => {
+  app.listen(port, () => {
+    console, console.log("Acessar servidor: http://localhost:3000/");
+    console.log("Servidor ativo na porta 3000");
+  });
 });
