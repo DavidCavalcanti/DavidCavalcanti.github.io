1b7510bd7e4171da86704b198790a4bd73feb3cd
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-07 11:11:30 -0300

    Usar sessão
    Sessão ficará viva durante 7 dias.

--- Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
+++ Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
@@ -8,10 +8,12 @@ HomeModel.find({
   .catch((erro) => console.log(erro));
 
 exports.paginaHome = (req, res) => {
+  req.session.usuario = { nome: "David", logado: true };  
   res.render("home.ejs");
   /*res.send(`
        <h1 style="text-align: center"> Hy, I'm David Cavalcanti 
         <br> Computer enginnering Student at UNIVASF        
         </h1><hr>                
     `);*/
+  return;
 };
