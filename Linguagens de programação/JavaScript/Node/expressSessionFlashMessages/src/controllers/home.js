13d9edf4c20759b5260105846f5cf98710baadc7
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-08 13:46:29 -0300

    Criar conteúdo para injetar na view

--- Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
+++ Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
@@ -8,10 +8,13 @@ HomeModel.find({
   .catch((erro) => console.log(erro));
 
 exports.paginaHome = (req, res) => {
-  req.flash("home", "Hello world!");
+  /*req.flash("home", "Hello world!");
   req.flash("info", "Página em manutenção");
-  req.flash("sucess", "Usuário logado");
-  res.render("home.ejs");
+  req.flash("sucess", "Usuário logado");*/
+  res.render("home.ejs", {
+    titulo: "Este será o título da página",
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  });
   /*res.send(`
        <h1 style="text-align: center"> Hy, I'm David Cavalcanti 
         <br> Computer enginnering Student at UNIVASF        
