20600c7fd537e969d7d5fe7f9cb04c830bee78ca
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-08 14:00:30 -0300

    Validar função inserida na view

--- Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
+++ Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/src/controllers/home.js
@@ -12,7 +12,7 @@ exports.paginaHome = (req, res) => {
   req.flash("info", "Página em manutenção");
   req.flash("sucess", "Usuário logado");*/
   res.render("home.ejs", {
-    titulo: "Este será o título da página",
+    //titulo: "Este será o título da página",
     numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   });
   /*res.send(`
