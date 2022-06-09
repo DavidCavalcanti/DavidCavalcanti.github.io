28b80973d58aa9bed3275884f6793d57024689c1
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-09 12:16:50 -0300

    Criar rota para tratar formulário

--- Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/routes.js
+++ Linguagens de programação/JavaScript/Node/expressSessionFlashMessages/routes.js
@@ -5,9 +5,10 @@ const paginaContato = require("./src/controllers/contato");
 
 // Rota da página home
 route.get("/", paginaHome.paginaHome);
+route.post("/", paginaHome.trataPost);
 
 // Rota da página contato
 route.get("/contato", paginaContato.paginaContato);
-route.post("/", paginaContato.trataForm);
+route.post("/contato", paginaContato.trataForm);
 
 module.exports = route;
