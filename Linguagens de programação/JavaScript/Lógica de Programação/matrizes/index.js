a293b0895115080cceb370e5c3e3ae1e53de0e90
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-21 16:41:12 -0300

    Ler conteúdo da matriz e imprimir

--- Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
+++ Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
@@ -2,6 +2,7 @@ var linhas = prompt("Digite a quantidade de linhas da matriz:");
 var colunas = prompt("Digite a quantidade de colunas da matriz:");
 alert(`Criar uma matriz com ${linhas} linhas e ${colunas} colunas`);
 
+// Criar matriz vazia
 var matriz = [];
 
 for (var i = 0; i < linhas; i++) {
@@ -11,8 +12,21 @@ for (var i = 0; i < linhas; i++) {
   }
 }
 
+//Popular matriz com entrada do usuário
 for (var L = 0; L < linhas; L++) {
   for (var C = 0; C < colunas; C++) {
     matriz[L][C] = prompt("Digite um valor para inserir na matriz:");
   }
 }
+
+// Ler conteúdo da matriz
+var valores = "";
+
+for (var L = 0; L < linhas; L++) {
+  for (var C = 0; C < colunas; C++) {
+    if (C < colunas - 1) valores += matriz[L][C] + "\t";
+    else valores += matriz[L][C] + "\n";
+  }
+}
+
+alert(`Conteúdo da matriz:\n${valores}`);
