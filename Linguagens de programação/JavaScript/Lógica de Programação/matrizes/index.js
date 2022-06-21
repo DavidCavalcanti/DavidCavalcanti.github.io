21e527d81af58dbce9126f1d4912bb877c35d31d
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-21 15:48:57 -0300

    Exibir matriz em tabela no terminal

--- Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
+++ Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
@@ -4,14 +4,4 @@ var matriz = [
   [17, 06],
 ];
 
-var valores = ""; // Vai receber os elementos da matriz
-
-// Este primeiro for varre as linhas da matriz
-for (var L = 0; L <= 2; L++) {
-  // Este segundo for varre as coluns da matriz
-  for (var C = 0; C <= 1; C++) {
-    valores += matriz[L][C] + "\n";
-  }
-}
-
-console.log(`conteúdo da matriz: ${valores}`);
+console.table(matriz);
