77eee0d81a0f3337fe6c4bc7a9d917cb5019215f
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-21 15:56:38 -0300

    Exibir matriz com forma de tabela
    Página web

--- Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
+++ Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
@@ -4,4 +4,15 @@ var matriz = [
   [17, 06],
 ];
 
-console.table(matriz);
+var valores = ""; // Vai receber os elementos da matriz
+
+// Este primeiro for varre as linhas da matriz
+for (var L = 0; L <= 2; L++) {
+  // Este segundo for varre as coluns da matriz
+  for (var C = 0; C <= 1; C++) {
+    if (C < 1) valores += matriz[L][C] + " \t";
+    else valores += matriz[L][C] + "\n";
+  }
+}
+
+alert(valores);
