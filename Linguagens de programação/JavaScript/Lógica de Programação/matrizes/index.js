cc8317eae94c8beb4a031ddd9abedba84e686dba
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-21 16:06:48 -0300

    Criar matriz vazia

--- Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
+++ Linguagens de programação/JavaScript/Lógica de Programação/matrizes/index.js
@@ -1,3 +1,12 @@
 var linhas = prompt("Digite a quantidade de linhas da matriz:");
 var colunas = prompt("Digite a quantidade de colunas da matriz:");
 alert(`Criar uma matriz com ${linhas} linhas e ${colunas} colunas`);
+
+var matriz = [];
+
+for (var i = 0; i < linhas; i++) {
+  matriz[i] = [];
+  for (var j = 0; j < colunas; j++) {
+    matriz[i][j] = "";
+  }
+}
